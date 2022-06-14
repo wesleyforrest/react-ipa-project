59a51cd79c95c4643f1a36df6a4f940fce1b6b51
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2022-06-14 16:59:54 +0100

    added search logic - not working yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { useState, useEffect } from "react";
 import "./App.scss";
 import Main from "./containers/Main/Main";
 import NavBar from "./containers/NavBar/NavBar";
-import beers from "./data/beers";
 
-function App() {
+function App(props) {
+  const { handleSubmit } = props;
+  useEffect(() => {
+    getBeers();
+  }, []);
+  const [searchInput, setSearchInput] = useState;
+
+  const [beers, setBeers] = useState();
+  const getBeers = async () => {
+    let url = "https://api.punkapi.com/v2/beers";
+    const res = await fetch(url);
+    const data = await res.json();
+    setBeers(data);
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      let input = event.target[0].value;
+
+      if (searchInput) {
+        event.target.reset();
+        setSearchInput(input);
+      }
+    };
+  };
+
   console.log(beers);
   return (
     <div className="App">
--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./SearchBox.scss";
 
-const SearchBox = () => {
+const SearchBox = (props) => {
+  const { handleSubmit } = props;
+  console.log(handleSubmit);
+
   return (
     <div className="search-box">
-      <input type="text" placeholder="Search..." />
+      <input type="text" placeholder="Search..." onInput={handleSubmit} />
     </div>
   );
 };
